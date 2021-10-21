96cce3d313cb9dab7f0447c790d3f4bd2e32c396
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-10-21 13:15:52 -0300

    Configuração do access_token do github e criação da migrate com prisma

--- node_heat/src/controllers/AuthenticateUserController.ts
+++ node_heat/src/controllers/AuthenticateUserController.ts
@@ -1,9 +1,9 @@
-import { Request, Response} from "express"
+import { Request, Response } from "express"
 import { AuthenticateUserService } from "../services/AuthenticateUserService"
 
 class AuthenticateUserController {
     async handle(request: Request, response: Response) {
-        const {code} = request.body
+        const { code } = request.body
 
         const service = new AuthenticateUserService()
         const result = await service.execute(code)
